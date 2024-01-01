d080cd349d541a9696c67892cbf5bb6d30ade1ed
farisubuntu <farisubunu@gmail.com>
2024-01-01 23:41:09 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import './App.css';
+import { useRef } from "react";
 
-function App() {
+export default function Form() {
+  const inputRef = useRef(null);
+  const commandRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+function handleChangeColor(){
+  commandRef.current.style.color="green";
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World - React</h1>
-      </header>
-    </div>
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          className="card-img-top"
+          alt="..."
+        />
+        <div ref={commandRef} className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="card-subtitle mb-2 text-muted ">Card subtitle</h6>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          b5
+        </div>
+        <button onClick={handleChangeColor}>Change color</button>
+      </div>
+    </>
   );
 }
 
-export default App;
+function App() {
+  return <Form />;
+}
--- src/useRef.md
+++ src/useRef.md
@@ -31,3 +31,61 @@ function App() {
 }
 
 ```
+**Dont read or write a `ref` during rendering**
+
+```js
+function MyComponent() {
+  // ...
+  // 🚩 Don't write a ref during rendering
+  myRef.current = 123;
+  // ...
+  // 🚩 Don't read a ref during rendering
+  return <h1>{myOtherRef.current}</h1>;
+}
+```
+
+__You can read or write refs from event handlers or effects instead__
+
+```js
+function MyComponent() {
+  // ...
+  useEffect(() => {
+    // ✅ You can read or write refs in effects
+    myRef.current = 123;
+  });
+  // ...
+  function handleClick() {
+    // ✅ You can read or write refs in event handlers
+    doSomething(myOtherRef.current);
+  }
+  // ...
+}
+```
+
+### Manipulating the DOM with a ref:
+
+```js
+
+import { useRef } from 'react';
+// first, declare it with initial value
+function MyComponent() {
+  const inputRef = useRef(null);
+  // ...
+  
+  // Then pass your ref object as the ref attribute to the JSX of the DOM node you want to manipulate:
+  return (
+    <>
+    <input ref={inputRef} />
+
+    </>
+  )
+  //...
+
+// React will set the current property of your ref object to that DOM node. 
+//+ Now you can access the <input>’s DOM node and call methods like focus():
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+```
