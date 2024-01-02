f64ffb34a32262341571abba407dbdcfbdd7d788
farisubuntu <farisubunu@gmail.com>
2024-01-02 19:02:17 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 
 function App() {
@@ -9,44 +8,5 @@ function App() {
   </h2>
   </>
   )
-=======
-import Heading from "./Heading";
-import Section from "./Section";
-import LevelContext from "./LevelContext";
-
-import "./App.css";
-
-function Page() {
-  return (
-    <Section>
-      <Heading level={1}>Title</Heading>
-      <Section level={2}>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Section level={3}>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Section level={4}>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
-    </Section>
-  );
-}
-
-function App() {
-  return (
-    <>
-     
-        <Page />
-  
-    </>
-  );
->>>>>>> context-1
 }
 export default App;
