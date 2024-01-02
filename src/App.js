9ff10511ad48d3fdda19516be059b1202b900256
farisubuntu <farisubunu@gmail.com>
2024-01-02 17:00:17 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import Page from "./Page";
+import Heading from "./Heading";
+import Section from "./Section";
+
 import "./App.css";
 
-
-
-
-
+function Page() {
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Section>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
 
 function App() {
-  return <Page />;
+  return (
+    <>
+      <Page />
+    </>
+  );
 }
 export default App;
