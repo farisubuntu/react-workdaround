cd7134a701ea42d0be9a60974d99645dec857255
farisubuntu <farisubunu@gmail.com>
2024-01-02 17:26:01 +0300

    finsi

--- src/Heading.js
+++ src/Heading.js
@@ -1,26 +1,5 @@
 import { useContext } from "react";
 import { LevelContext } from "./LevelContext";
-<<<<<<< HEAD
-
-function Heading({ children }) {
-  const level = useContext(LevelContext);
-  switch (level) {
-    case 1:
-      return <h1>{children}</h1>;
-    case 2:
-      return <h2>{children}</h2>;
-    case 3:
-      return <h3>{children}</h3>;
-    case 4:
-      return <h4>{children}</h4>;
-    case 5:
-      return <h5>{children}</h5>;
-    case 6:
-      return <h6>{children}</h6>;
-    default:
-      throw Error("Unknown level: " + level);
-  }
-=======
 import "./Heading.css";
 
 
@@ -42,7 +21,6 @@ function Heading({children }) {
    default:
      console.log('not known.....');
  }
->>>>>>> context-2
 }
 
 export default Heading;
--- src/LevelContext.js
+++ src/LevelContext.js
@@ -1,9 +1,4 @@
 import { createContext } from "react";
 
-<<<<<<< HEAD
-
-export const LevelContext = createContext(1);
-=======
 export const LevelContext=createContext(1);
 
->>>>>>> context-2
