59a79b902a15e668e7ba9ccf04769b531dff40ab
farisubuntu <farisubunu@gmail.com>
2024-01-02 17:26:15 +0300

    finish

--- src/Section.js
+++ src/Section.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { useContext } from "react";
-import { LevelContext } from "./LevelContext";
-
-
-function Section({children }) {
- const level= useContext(LevelContext);
-  return (
-    <section className="section">
-      <LevelContext.Provider value={level}>
-      {children}
-      </LevelContext.Provider>
-    </section>
-  );
-=======
 import { LevelContext } from "./LevelContext";
 
 function Section({level, children }) {
@@ -25,7 +10,6 @@ function Section({level, children }) {
     </LevelContext.Provider>
    </section>
  );
->>>>>>> context-2
 }
 
 export default Section;
