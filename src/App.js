19b54d853eb041f4199e2acc9f5849a14814dd1d
farisubuntu <farisubunu@gmail.com>
2024-01-03 23:26:24 +0300

    sfd

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
-
-function VideoPlayer({ src, isPlaying }) {
-  const ref = useRef(null);
-
+function EffectsDemoTwoStates() {
+  const [title, setTitle] = useState("default title");
+  const titleRef = useRef();
+  const [darkMode, setDarkMode] = useState(false);
   useEffect(() => {
-    if (isPlaying) {
-      ref.current.play();
-    } else {
-      ref.current.pause();
-    }
+    console.log("useEffect");
+    document.title = title;
   });
-
-  return <video ref={ref} src={src} loop playsInline />;
+  console.log("render");
+  const handleClick = () => setTitle(titleRef.current.value);
+  const handleCheckboxChange = () => setDarkMode((prev) => !prev);
+  return (
+    <div className={darkMode ? "dark-mode" : ""}>
+      <label htmlFor="darkMode">dark mode</label>
+      <input
+        name="darkMode"
+        type="checkbox"
+        checked={darkMode}
+        onChange={handleCheckboxChange}
+      />
+      <input ref={titleRef} />
+      <button onClick={handleClick}>change title</button>
+    </div>
+  );
 }
 
 export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <>
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
+      <h3>Hello</h3>
+      <hr />
+      <EffectsDemoTwoStates />
     </>
   );
 }
