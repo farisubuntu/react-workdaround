d2557bfa9eb42733f91201a1018d25c7798c553f
farisubuntu <farisubunu@gmail.com>
2024-01-05 02:36:18 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import "./App.css";
-import {useContext} from "react";
+import { createContext, useContext } from "react";
 
+const ThemeContext = createContext(null);
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = "button-" + theme;
+  return <button className={className}>{children}</button>;
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = "panel-" + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  );
+}
+
+function Form() {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+}
 
 function App() {
-  return(
-  <>
-  <h2>Usage</h2>
-  </>
-  )
+  return (
+    <ThemeContext.Provider value="dark">
+      <Form />
+    </ThemeContext.Provider>
+  );
 }
+
 export default App;
