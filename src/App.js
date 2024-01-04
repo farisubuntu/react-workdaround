01c4fb4e1d70dc513969388f82570f0b936f9f7f
farisubuntu <farisubunu@gmail.com>
2024-01-04 16:08:00 +0300

    sadf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+import _react_lifecycles from "./react-lifecycles.png";
 
+function Badge() {
+  const [count, setCount] = useState(0);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    console.log("useEffect .......");
+  }, []);
+
+  useEffect(()=>{
+    inputRef.current.addEventListener('click',handleAlertClick)
+
+  },[])
+
+  function handleChange(e) {
+    const num = e.target.value.length;
+    setCount(num);
+  }
+function handleAlertClick(e){
+   e.target.style.backgroundColor='green'
+}
+  return (
+    <>
+      <div className="input-data">
+        <label htmlFor="name">Count characters </label>
+        <input
+          className="form-control w-50 primary yellow"
+          type="text"
+          id="name"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="center" ref={inputRef}>
+        <div className="alert alert-success w-25 p-0 text-center my-3">
+          {count}
+        </div>
+      </div>
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World - React</h1>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Badge />
+      </div>
+
+      <hr />
+      <img
+        src={_react_lifecycles}
+        className="img-fluid"
+        alt="react lifecycles"
+      />
+    </>
   );
 }
 
