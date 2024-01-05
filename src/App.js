478dbf67beff6e1fb4120a6319bc83249d1fd5b8
farisubuntu <farisubunu@gmail.com>
2024-01-05 20:51:47 +0300

    asdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import { useState } from 'react';
-import { useWindowListener } from './useWindowListener.js';
+import Box from './Box.js';
 
 export default function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  useWindowListener('pointermove', (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-  });
-
   return (
-    <div style={{
-      position: 'absolute',
-      backgroundColor: 'pink',
-      borderRadius: '50%',
-      opacity: 0.6,
-      transform: `translate(${position.x}px, ${position.y}px)`,
-      pointerEvents: 'none',
-      left: -20,
-      top: -20,
-      width: 40,
-      height: 40,
-    }} />
+    <>
+      <LongSection />
+      <Box />
+      <LongSection />
+      <Box />
+      <LongSection />
+    </>
   );
+}
+
+function LongSection() {
+  const items = [];
+  for (let i = 0; i < 50; i++) {
+    items.push(<li key={i}>Item #{i} (keep scrolling)</li>);
+  }
+  return <ul>{items}</ul>
 }
