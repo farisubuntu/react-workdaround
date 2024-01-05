59aac9c5783d8dd90fbf71d1dc455791ab6cd44a
farisubuntu <farisubunu@gmail.com>
2024-01-05 03:37:33 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import { createContext, useContext } from "react";
+
+const AppContext = createContext(null);
+
+function ButtonGroup({ title }) {
+  const appContext = useContext(AppContext);
+  let buttons = Array(appContext);
+  console.log(appContext, "and ", buttons);
+
+  return;
+  <div>
+    {buttons.map((x) => {
+      return <button>{title}</button>;
+    })}
+  </div>;
+}
 
 function App() {
-  return(
-  <>
-  </>
-  )
+  return (
+    <>
+      <AppContext.Provider value={4}>
+        <ButtonGroup title="a button" />
+      </AppContext.Provider>
+    </>
+  );
 }
 export default App;
