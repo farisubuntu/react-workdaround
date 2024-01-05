9b2cee5e37d178a6bbef4bebc6a5ba5dfad1a0c9
farisubuntu <farisubunu@gmail.com>
2024-01-05 04:03:54 +0300

    sdfs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import "./App.css";
-import {useContext} from "react";
+import { createContext, useContext, useState } from 'react';
 
+const ThemeContext = createContext(null);
 
 function App() {
-  return(
-  <>
-  <h2>Usage</h2>
-  </>
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <Form />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === 'dark'}
+          onChange={(e) => {
+            setTheme(e.target.checked ? 'dark' : 'light')
+          }}
+        />
+        Use dark mode
+      </label>
+    </ThemeContext.Provider>
   )
 }
+
+function Form({ children }) {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'button-' + theme;
+  return (
+    <button className={className}>
+      {children}
+    </button>
+  );
+}
+
+
 export default App;
