8e8778c362f443d016743809811e094d12d53bf3
farisubuntu <farisubunu@gmail.com>
2024-01-05 20:49:13 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import { useState } from 'react';
-import { useChatRoom } from './useChatRoom.js';
+import { useWindowListener } from './useWindowListener.js';
 
-function ChatRoom({ roomId }) {
-  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+export default function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useChatRoom({
-    roomId: roomId,
-    serverUrl: serverUrl
+  useWindowListener('pointermove', (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
   });
 
   return (
-    <>
-      <label>
-        Server URL:{' '}
-        <input
-          value={serverUrl}
-          onChange={e => setServerUrl(e.target.value)}
-        />
-      </label>
-      <h1>Welcome to the {roomId} room!</h1>
-    </>
-  );
-}
-
-export default function App() {
-  const [roomId, setRoomId] = useState('general');
-  const [show, setShow] = useState(false);
-  return (
-    <>
-      <label>
-        Choose the chat room:{' '}
-        <select
-          value={roomId}
-          onChange={e => setRoomId(e.target.value)}
-        >
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="music">music</option>
-        </select>
-      </label>
-      <button onClick={() => setShow(!show)}>
-        {show ? 'Close chat' : 'Open chat'}
-      </button>
-      {show && <hr />}
-      {show && <ChatRoom roomId={roomId} />}
-    </>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'pink',
+      borderRadius: '50%',
+      opacity: 0.6,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+    }} />
   );
 }
